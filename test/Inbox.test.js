11cec576c9b5a05691a809764028e1dbd6bca1d7
KseniiaRyuma <kseniya0213@gmail.com>
2018-02-12 23:19:11 -0800

    test instance

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,5 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constractor
+const web3 = new Web3(ganache.provider()); // creates an instance of Web3 and tell to create a local test network that we are hosting in our machine
+
